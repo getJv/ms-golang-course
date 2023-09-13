5043e95f79aff046a46dae83c17b65bd1c09c6c3
Jhonatan Morais <jhonatanvinicius@gmail.com>
2023-09-13 10:35:36 +0200

    class 31 done

---
+++ logger-service/cmd/api/handlers.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"logger/data"
+	"net/http"
+)
+
+type JsonPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data "
+	`
+}
+
+func (app *Config) WriteLog(w http.ResponseWriter, r *http.Request) {
+	var requestPayload JsonPayload
+
+	_ = app.ReadJson(w, r, &requestPayload)
+
+	event := data.LogEntry{
+		Name: requestPayload.Name,
+		Data: requestPayload.Data,
+	}
+
+	err := app.Models.LogEntry.Insert(event)
+	if err != nil {
+		app.ErrorJson(w, err)
+		return
+	}
+
+	resp := jsonResponse{
+		Error:   false,
+		Messa ge: "logged",
+	}
+
+	app.WriteJson(w, http.StatusAccepted, resp)
+
+}
